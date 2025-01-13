6a1470c82e1845ec4a33cd3c3e7d282d8ffce3f3
Sangam <np03cs4a230102@heraldcollege.edu.np>
2025-01-13 10:29:49 +0545

    added getter and setter methods in the file

--- Student.java
+++ Student.java
@@ -5,4 +5,16 @@ public class Student {
             this.name=name;
             this.age=age;
         }
+        void setName(String name){
+            this.name=name;
+        }
+        void setAge(int age){
+            this.age=age;
+        }
+        String getName(){
+            return this.name;
+        }
+        int getAge(){
+            return this.age;
+        }
 }
